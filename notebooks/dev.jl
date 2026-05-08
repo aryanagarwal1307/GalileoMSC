222f5fe134896ab4f8e9f23b30bbd912de8122b7
Aryan Agarwal <aa2842@login2.misha.ycrc.yale.edu>
2026-05-08 12:55:23 -0400

    project-setup

---
+++ notebooks/dev.jl
@@ -0,0 +1,21 @@
+### A Pluto.jl notebook ###
+# v0.20.24
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c03fbc42-3f22-11f1-861b-2b93190948b7
+begin
+	using Pkg
+	Pkg.activate(joinpath(@__DIR__, ".."))
+	
+	using Revise
+	using GalileoMSC
+end
+
+# ╔═╡ 2b279fec-c696-4f3c-9b6d-f342fc393867
+
+
+# ╔═╡ Cell order:
+# ╠═c03fbc42-3f22-11f1-861b-2b93190948b7
+# ╠═2b279fec-c696-4f3c-9b6d-f342fc393867
---
+++ src/GalileoMSC.jl
@@ -0,0 +1,5 @@
+module GalileoMSC
+
+greet() = print("Hello World!")
+
+end # module GalileoMSC
